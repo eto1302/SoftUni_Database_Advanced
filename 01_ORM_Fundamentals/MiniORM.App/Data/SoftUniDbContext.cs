27a617485d6f3a5a823fda5c82709de9a1b971c7
eto1302 <martinangelov1302@gmail.com>
2019-02-28 10:38:28 +0200

    This is the exercise from the ORM Fundamentals lecture of the SoftUni course "Databases Advanced"

---
+++ 01_ORM_Fundamentals/MiniORM.App/Data/SoftUniDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App.Data
+{
+    public class SoftUniDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments{ get; set; }
+        public DbSet<Project> Projects{ get; set; }
+        public DbSet<EmployeeProject> EmployeesProjects{ get; set; }
+
+        public SoftUniDbContext(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
---
+++ 01_ORM_Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=.\\SQLEXPRESS;Database=MinionsDB;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployeed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
