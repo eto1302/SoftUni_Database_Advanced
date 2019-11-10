f6dad404aa93e6fd254f0b127a878c633d49b930
eto1302 <martinangelov1302@gmail.com>
2019-11-10 16:14:23 +0200

    This is the XML processing exercise from the SoftUni course Databases Advanced

---
+++ 10_XML_Processing/ProductShop/ProductShop/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        public Category()
+        {
+            this.CategoryProducts = new List<CategoryProduct>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public ICollection<CategoryProduct> CategoryProducts { get; set; }
+    }
+}
---
+++ 10_XML_Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(User[]), new XmlRootAttribute("Users"));
+
+            var Users =
+                (User[]) serializer.Deserialize(new StringReader(inputXml));
+
+            context.Users.AddRange(Users);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {Users.Length}";
+
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(Product[]), new XmlRootAttribute("Products"));
+
+            var Products =
+                (Product[])serializer.Deserialize(new StringReader(inputXml));
+
+            context.Products.AddRange(Products);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {Products.Length}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(Category[]));
+
+            var Categories =
+                (Category[]) serializer.Deserialize(new StringReader(inputXml));
+
+            context.Categories.AddRange(Categories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {Categories.Length}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(CategoryProduct[]));
+
+            var CategoryProducts = (CategoryProduct[]) serializer.Deserialize(new StringReader(inputXml));
+
+            return $"Successfully imported {CategoryProducts.Length}";
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var serializer = new XmlSerializer(typeof(Product[]), new XmlRootAttribute("Products"));
+            var Products = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new
+                {
+                    name = p.Name,
+                    price = p.Price,
+                    buyer = p.Buyer.FirstName + " " + p.Buyer.LastName
+                }).ToArray();
+
+            var sb = new StringBuilder();
+            var xmlNamespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            serializer.Serialize(new StringWriter(sb), Products, xmlNamespaces);
+            return sb.ToString();
+        }
+    }
+}
